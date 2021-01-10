f2dc996bc40602f287ee3ce7b437a07f0dab293c
AdriNarendra <50900382+AdriNarendra@users.noreply.github.com>
2021-01-10 19:17:16 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,31 +3,8 @@ import pandas as pd
 import xgboost as xgb
 from flask import Flask, request, jsonify, render_template
 import pickle
-from sklearn.preprocessing import OneHotEncoder as SklearnOneHotEncoder
-class OneHotEncoder(SklearnOneHotEncoder):
-    def __init__(self, **kwargs):
-        super(OneHotEncoder, self).__init__(**kwargs)
-        self.fit_flag = False
-    def fit(self, X, **kwargs):
-        out = super().fit(X)
-        self.fit_flag = True
-        return out
-    def transform(self, X, **kwargs):
-      sparse_matrix = super(OneHotEncoder, self).transform(X)
-      new_columns = self.get_new_columns(X=X)
-      d_out = pd.DataFrame(sparse_matrix.toarray(), columns=new_columns, index=X.index)
-      return d_out
-    def get_new_columns(self, X):
-      new_columns = []
-      for i, column in enumerate(X.columns):
-          j = 0
-          while j < len(self.categories_[i]):
-            new_columns.append(f'{column}_<{self.categories_[i][j]}>')
-            j += 1
-      return new_columns
-    def fit_transform(self, X, **kwargs):
-      self.fit(X)
-      return self.transform(X)
+import sklearn
+from ohe import OneHotEncoder
 
 app = Flask(__name__)
 model = pickle.load(open('xgb_model3.pkl', 'rb'))
