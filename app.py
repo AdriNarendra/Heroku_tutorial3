f8555416ce6fb663de3a4e2ac6b68b253bc82ba0
AdriNarendra <50900382+AdriNarendra@users.noreply.github.com>
2021-01-10 17:27:11 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+import xgboost as xgb
 from flask import Flask, request, jsonify, render_template
 import pickle
 
