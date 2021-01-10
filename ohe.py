e8ef1b5effa74428ae2c748910a8709880d198aa
AdriNarendra <50900382+AdriNarendra@users.noreply.github.com>
2021-01-10 19:27:46 +0700

    Add files via upload

--- ohe.py
+++ ohe.py
@@ -1,58 +1,25 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from sklearn.preprocessing import OneHotEncoder as SklearnOneHotEncoder\n",
-    "class OneHotEncoder(SklearnOneHotEncoder):\n",
-    "    def __init__(self, **kwargs):\n",
-    "        super(OneHotEncoder, self).__init__(**kwargs)\n",
-    "        self.fit_flag = False\n",
-    "    def fit(self, X, **kwargs):\n",
-    "        out = super().fit(X)\n",
-    "        self.fit_flag = True\n",
-    "        return out\n",
-    "    def transform(self, X, **kwargs):\n",
-    "      sparse_matrix = super(OneHotEncoder, self).transform(X)\n",
-    "      new_columns = self.get_new_columns(X=X)\n",
-    "      d_out = pd.DataFrame(sparse_matrix.toarray(), columns=new_columns, index=X.index)\n",
-    "      return d_out\n",
-    "    def get_new_columns(self, X):\n",
-    "      new_columns = []\n",
-    "      for i, column in enumerate(X.columns):\n",
-    "          j = 0\n",
-    "          while j < len(self.categories_[i]):\n",
-    "            new_columns.append(f'{column}_<{self.categories_[i][j]}>')\n",
-    "            j += 1\n",
-    "      return new_columns\n",
-    "    def fit_transform(self, X, **kwargs):\n",
-    "      self.fit(X)\n",
-    "      return self.transform(X)"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.6"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+from sklearn.preprocessing import OneHotEncoder as SklearnOneHotEncoder
+class OneHotEncoder(SklearnOneHotEncoder):
+    def __init__(self, **kwargs):
+        super(OneHotEncoder, self).__init__(**kwargs)
+        self.fit_flag = False
+    def fit(self, X, **kwargs):
+        out = super().fit(X)
+        self.fit_flag = True
+        return out
+    def transform(self, X, **kwargs):
+      sparse_matrix = super(OneHotEncoder, self).transform(X)
+      new_columns = self.get_new_columns(X=X)
+      d_out = pd.DataFrame(sparse_matrix.toarray(), columns=new_columns, index=X.index)
+      return d_out
+    def get_new_columns(self, X):
+      new_columns = []
+      for i, column in enumerate(X.columns):
+          j = 0
+          while j < len(self.categories_[i]):
+            new_columns.append(f'{column}_<{self.categories_[i][j]}>')
+            j += 1
+      return new_columns
+    def fit_transform(self, X, **kwargs):
+      self.fit(X)
+      return self.transform(X)
