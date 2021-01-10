aa07bcec961a900f61a3bca4cef14ddc3242d44b
AdriNarendra <50900382+AdriNarendra@users.noreply.github.com>
2021-01-11 00:35:29 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import xgboost as xgb
 from flask import Flask, request, jsonify, render_template
 import pickle
 import sklearn
-from ohe import OneHotEncoder
+from ohe import OneHotEncoder, dataframe
+from pandas import DataFrame
 
 app = Flask(__name__)
 model = pickle.load(open('xgb_model3.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pkl', 'rb'))
-encoder = pickle.load(open('one_hot_encoder.pkl', 'rb'))
+encoder = pickle.load(open('one_hot_encoder (2).pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -27,11 +28,11 @@ def predict():
     categorical_values = ['Gender','Married','Dependents','Education','Self_Employed','Credit_History','Property_Area']
     cat_features = [request.form.get(key) for key in categorical_values]
     int_features = [int(request.form.get(key)) for key in numerical_values]
-    cat_data = pd.DataFrame(columns=list(categorical_values))
-    cat_data.loc[0] = cat_features
-    ohe = encoder.transform(cat_data)
+    data = dataframe(categorical_values,cat_features)
+    ohe = encoder.transform(np.array(cat_features).reshape(1,-1))
     scaled_features = scaler.transform(np.array(int_features).reshape(1,-1))
-    frame = [scaled_features,ohe]
+    num_data = DataFrame(scaled_features)
+    frame = [num_data,ohe]
     final_df = pd.concat(frame,axis=1)
     final_features = np.array(final_df)
     prediction = model.predict(final_features)
@@ -45,5 +46,6 @@ def predict():
 
 
 if __name__ == "__main__":
-    port-int(os.environ.get('PORT',5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    #port-int(os.environ.get('PORT',5000))
+    #app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
